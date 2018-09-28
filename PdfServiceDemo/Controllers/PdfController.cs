8289a2082e28a530dc7766d8e01db3ff79c3dae9
Marcel Studer <marcel.studer@uni.li>
2018-09-28 09:52:02 +0200

    Created demo project

---
+++ PdfServiceDemo/Controllers/PdfController.cs
@@ -0,0 +1,18 @@
+﻿using EvoPdf;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PdfServiceDemo.Controllers
+{
+    [ApiController]
+    [Route("api/getpdf")]
+    public class PdfController : ControllerBase
+    {
+
+        public IActionResult GetPdf()
+        {
+            var htmlToPdfConverter = new HtmlToPdfConverter();
+            byte[] outPdfBuffer = htmlToPdfConverter.ConvertUrl("https://www.evopdf.com/buy.aspx");
+            return File(outPdfBuffer, "application/pdf", "file.pdf");
+        }
+    }
+}
